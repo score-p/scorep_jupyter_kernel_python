8777418b0ab4601d56315b13ad9b470003d24b7d
Elias Werner <eliwerner3@googlemail.com>
2022-09-26 12:24:10 +0200

    use string for userpers instead of file

--- src/scorep_jupyter/userpersistence.py
+++ src/scorep_jupyter/userpersistence.py
@@ -5,6 +5,7 @@ import inspect
 import shutil
 import dill
 import sys
+
 '''
 Note: general limitation
 changes in modules can not be persisted since pickling/shelving modules is not allowed
@@ -12,14 +13,10 @@ this might be fixed by using shared memory
 '''
 
 
-def save_user_definitions(code, tmp_user_pers_file):
+def save_user_definitions(code):
     # keep imports, classes and definitions
     root = ast.parse(code)
-    '''
-    code_curr = [node for node in ast.iter_child_nodes(root) if <isinstance(node, ast.FunctionDef) or
-                 isinstance(node, ast.AsyncFunctionDef) or isinstance(node, ast.ClassDef) or
-                 isinstance(node, ast.Import) or isinstance(node, ast.ImportFrom)]
-    '''
+
     code_curr = []
     for top_node in ast.iter_child_nodes(root):
         if isinstance(top_node, ast.With):
@@ -32,58 +29,29 @@ def save_user_definitions(code, tmp_user_pers_file):
               isinstance(top_node, ast.ClassDef) or isinstance(top_node, ast.Import) or isinstance(top_node,
                                                                                                    ast.ImportFrom)):
             code_curr.append(top_node)
-    '''
-    # keep changed attributes (either assignments or expressions)
-    TODO: needs some more extensive handling to distinguish between modules and object instances
-    attributes = []
-    for node in ast.iter_child_nodes(root):
-        # assignment nodes can include attributes, therefore go over all targets and check for attribute nodes
-        if isinstance(node, ast.Assign) or isinstance(node, ast.AnnAssign):
-            for el in node.targets:
-                for target_node in ast.walk(el):
-                    if isinstance(target_node, ast.Attribute):
-                        attributes.append(node)
-                        break
-        elif (isinstance(node, ast.Expr) and isinstance(node.value, ast.Call)
-              and isinstance(node.value.func, ast.Attribute)):
-            # attributes can also be set via function calls
-            attributes.append(node)
-    code_curr.extend(attributes)
-    code_curr.sort(key=lambda node: node.lineno)
-    '''
+
     code_curr_id = [node.name for node in code_curr if isinstance(node, ast.FunctionDef)
                     or isinstance(node, ast.ClassDef)]
 
     code_prev = []
-    if os.path.isfile(tmp_user_pers_file):
-        shutil.copyfile(tmp_user_pers_file, tmp_user_pers_file + "_backup")
-        with open(tmp_user_pers_file, "r") as f:
-            code_prev = [node for node in ast.iter_child_nodes(ast.parse(f.read()))]
 
-    # TODO: use shared memory
-    with open(tmp_user_pers_file, "w") as f:
-
-        '''
-        import_nodes = [node.names for node in code_curr if isinstance(node, ast.Import)]
-        for import_smt in import_nodes:
-            for imp in import_smt:
-                code_curr_id.append(imp.name)
-        '''
-
-        code_to_keep = []
-        for node in code_prev:
-            # TODO: maintain synch of imports
-            # TODO: maintain synch of attributes (del keyword)
-            if not (isinstance(node, ast.FunctionDef) or isinstance(node, ast.ClassDef) or
-                    isinstance(node, ast.AsyncFunctionDef)):
+    code_to_keep = []
+    for node in code_prev:
+        # TODO: maintain synch of imports
+        # TODO: maintain synch of attributes (del keyword)
+        if not (isinstance(node, ast.FunctionDef) or isinstance(node, ast.ClassDef) or
+                isinstance(node, ast.AsyncFunctionDef)):
+            code_to_keep.append(node)
+        else:
+            if node.name not in code_curr_id:
                 code_to_keep.append(node)
-            else:
-                if node.name not in code_curr_id:
-                    code_to_keep.append(node)
-        code_to_keep.append(code_curr)
+    code_to_keep.append(code_curr)
 
-        for node in code_to_keep:
-            f.write(astunparse.unparse(node))
+    pers_string = ""
+    for node in code_to_keep:
+        pers_string += astunparse.unparse(node)
+
+    return pers_string
 
 
 def get_user_variables_from_code(code):
@@ -103,18 +71,7 @@ def get_user_variables_from_code(code):
     return variables
 
 
-def save_user_variables(globs, variables, tmp_user_pers_file, tmp_user_vars_file):
-
-    if os.path.isfile(tmp_user_pers_file):
-        # remove ".py" to retrieve the module name
-        tmp_user_pers_mod = str(tmp_user_pers_file[:-3])
-        tmp_user_pers_mod = tmp_user_pers_mod.split("/")[1]
-        # dynamically load user defined classes to make them available for variable/object storing
-
-        _tmp = __import__(tmp_user_pers_mod, globals(), locals(), ['*'], 0)
-        for member in inspect.getmembers(_tmp):
-            if not str(member[0]).startswith("__"):
-                globals()[member[0]] = member[1]
+def save_user_variables(globs, variables, tmp_user_vars_file):
 
     prior_variables = load_user_variables(tmp_user_vars_file)
     user_variables = {k: v for k, v in globs.items() if str(k) in variables}
@@ -129,28 +86,15 @@ def save_user_variables(globs, variables, tmp_user_pers_file, tmp_user_vars_file
                 non_persistent_class = user_variables[el].__class__.__name__
                 if non_persistent_class in globals().keys():
                     user_variables[el].__class__ = globals()[non_persistent_class]
-                #d[el] = user_variables[el]
+                # d[el] = user_variables[el]
             dill.dump(user_variables, dill_file)
-            #d.close()
-
-
-def restore_user_definitions(tmp_user_pers_file):
-    if os.path.isfile(tmp_user_pers_file):
-        os.remove(tmp_user_pers_file)
-    if os.path.isfile(tmp_user_pers_file + "_backup"):
-        os.rename(tmp_user_pers_file + "_backup", tmp_user_pers_file)
+            # d.close()
 
 
 def load_user_variables(tmp_user_vars_file):
     user_variables = {}
     # TODO: use shared memory
-    '''
-    d = shelve.open(tmp_user_vars_file)
-    klist = list(d.keys())
-    for key in klist:
-        user_variables[key] = d[key]
-    d.close()
-    '''
+
     if os.path.isfile(tmp_user_vars_file):
         with open(tmp_user_vars_file, "rb") as dill_file:
             user_variables = dill.load(dill_file)
@@ -158,12 +102,8 @@ def load_user_variables(tmp_user_vars_file):
     return user_variables
 
 
-def tidy_up(tmp_user_pers_file, tmp_user_vars_file):
+def tidy_up(tmp_user_vars_file):
     try:
-        if os.path.isfile(tmp_user_pers_file):
-            os.remove(tmp_user_pers_file)
-        if os.path.isfile(tmp_user_pers_file + "_backup"):
-            os.remove(tmp_user_pers_file + "_backup")
         if os.path.isfile(tmp_user_vars_file):
             os.remove(tmp_user_vars_file)
     except:
