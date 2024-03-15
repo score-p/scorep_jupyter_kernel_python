bc22a95dbe240fbb0689ed22d6c37a760f071c03
s4122485--tu-dresden.de <elias.werner@tu-dresden.de>
2024-03-15 10:24:07 +0100

    fix install script

--- src/pyperf_jupyter/install.py
+++ src/pyperf_jupyter/install.py
@@ -6,8 +6,8 @@ import json
 
 kernel_spec = {
     "argv": [sys.executable, "-m", "pyperf_jupyter.kernel", "-f", "{connection_file}"],
-    "name": "pyperf",
-    "display_name": "PyPerf",
+    "name": "pyperfCPython",
+    "display_name": "PyPerfCPython",
     "language": "python"
 }
 
@@ -16,8 +16,8 @@ def install_kernel_spec():
     with TemporaryDirectory() as d:
         with open(os.path.join(d, 'kernel.json'), 'w') as f:
             json.dump(kernel_spec, f, sort_keys=True)
-        print("installing the pyperf_jupyter kernel")
-        KernelSpecManager().install_kernel_spec(d, 'PyPerf', user=True)
+        print("installed the pyperf_jupyter kernel")
+        KernelSpecManager().install_kernel_spec(d, 'PyPerfCPython', user=True)
 
 
 if __name__ == '__main__':
